889870a67278168462066f48f8ae462b8e13b855
Shiku1228 <latanggar2@gmail.com>
2026-01-24 23:15:48 +0800

    Added the toast function for the Buttons. Not yet relies on the question even if it is right or wrong.

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.geoquiz;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,36 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView questionTextView;
+    private Button trueButton;
+    private Button falseButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        // CONNECT XML TO JAVA MECHANICS
+        questionTextView = findViewById(R.id.question_text_view);
+        trueButton = findViewById(R.id.true_button);
+        falseButton = findViewById(R.id.false_button);
+
+        // TRUE BUTTON
+        trueButton.setOnClickListener(v ->
+                Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
+        );
+
+        // FALSE BUTTON
+        falseButton.setOnClickListener(v ->
+                Toast.makeText(this, "Wrong!", Toast.LENGTH_SHORT).show()
+        );
     }
 }
